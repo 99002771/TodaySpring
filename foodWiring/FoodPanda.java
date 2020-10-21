f182c15a51625b4568186ee34bf892444c91aa68
99002771 <72496320+99002771@users.noreply.github.com>
2020-10-21 18:07:59 +0530

    Add files via upload

---
+++ foodWiring/FoodPanda.java
@@ -0,0 +1,23 @@
+package com.example.foodWiring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+@Component
+public class FoodPanda {
+		@Autowired
+		@Qualifier("chinese")
+		Menu cmenu;
+		@Autowired
+		@Qualifier("indian")
+		Menu imenu;
+		
+		public void showMenu(String choice) {
+		
+		if(choice.equals("chinese"))
+			System.out.println(cmenu.itemsAvailable());
+		else
+			System.out.println(imenu.itemsAvailable());
+		}
+
+}
---
+++ foodWiring/Menu.java
@@ -0,0 +1,8 @@
+package com.example.foodWiring;
+
+import java.util.List;
+
+public interface Menu {
+	List<String> itemsAvailable();
+
+}
